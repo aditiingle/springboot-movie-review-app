002f6743a6b8379edd61085fdbd4c9d530f13d52
aditiingle <aditiingle14@gmail.com>
2025-04-05 09:45:40 -0400

    Fix line endings for mvnw.cmd

---
+++ movies/src/test/java/com/aditi/movie_review/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditi.movie_review.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
